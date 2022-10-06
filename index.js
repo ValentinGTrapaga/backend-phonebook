63a102f6f19df87467711e2c26842fbc40b7b9cd
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2022-10-05 21:15:25 -0300

    add mongodb

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
 
+const Person = require('./modules/person')
+const { response } = require('express')
+
 morgan.token('body', function (req, res) {
   return JSON.stringify(req.body)
 })
@@ -10,40 +13,18 @@ morgan.token('body', function (req, res) {
 app.use(express.json())
 app.use(
   morgan(
-    ':method :url :status :res[content-length] - :response-time ms - :body'
+    ':method :url :status :res[content-length] - :response-time ms - body: :body'
   )
 )
 app.use(cors())
-
-let phones = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456'
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523'
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345'
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122'
-  }
-]
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello from the phonebook server</h1>')
 })
 
-app.get('/info', (req, res) => {
-  const length = phones.length
+app.get('/info', async (req, res) => {
+  const data = await Person.find({})
+  const length = data.length
   const reqDate = new Date()
   res.send(
     `<h3>Phonebook has ${length} entries</h3>
@@ -53,40 +34,46 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-  res.status(200).json(phones)
+  Person.find({}).then((result) => {
+    res.json(result)
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = phones.find((person) => person.id === id)
-  person ? res.json(person) : res.status(404).end()
+  const id = req.params.id
+  Person.findById(id)
+    .then((person) => {
+      person ? res.json(person) : res.status(404).end()
+    })
+    .catch((err) => {
+      console.error(err)
+      res.status(500).end()
+    })
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  phones = phones.filter((person) => person.id !== id)
-  res.json(phones)
-})
-
-const generateId = () => {
-  const maxId = phones.length > 0 ? Math.max(...phones.map((n) => n.id)) : 0
-  return maxId + 1
-}
+// app.delete('/api/persons/:id', (req, res) => {
+//   const id = Number(req.params.id)
+//   phones = phones.filter((person) => person.id !== id)
+//   res.json(phones)
+// })
 
 app.post('/api/persons', (req, res) => {
   const body = req.body
-  const id = generateId()
-  const personToAdd = { ...body, id }
-  const existsName = phones.find(
-    (person) => JSON.stringify(person.name) === JSON.stringify(personToAdd.name)
-  )
-  if (body.name === '' || body.number === '') {
-    return res.status(400).json({ error: 'Content missing' })
-  } else if (existsName !== undefined) {
-    return res.status(400).json({ error: 'Name already exists' })
+  console.log(body)
+
+  if (!body) {
+    res.status(400).json({ error: 'Content missing' })
   }
-  phones = [...phones, personToAdd]
-  return res.status(202).json(phones)
+
+  const newPerson = new Person({
+    name: body.name,
+    number: body.number,
+    date: new Date()
+  })
+
+  newPerson.save().then((savedPerson) => {
+    res.json(savedPerson)
+  })
 })
 
 app.put('/api/persons/:id', (req, res) => {
